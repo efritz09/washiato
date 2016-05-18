534281a5267eef54b5ad3beda578c87514659ea1
Tom Owlett <tomowlett@gmail.com>
2016-05-17 23:04:19 -0700

    Added firebase stuff to check "on my way" and tell app how long it has been since cycle ended

--- Embedded/Code/Code.ino
+++ Embedded/Code/Code.ino
@@ -10,29 +10,42 @@
 
 #define BACKLIGHT_PIN 15
 
+// Machine States
 #define MACHINE_UNOCCUPIED 0
 #define MACHINE_WASHING 1
 #define MACHINE_FINISHED 2
 
-#define Window_Samples 1000
-#define UPPER_ACCEL_THRESHOLD 1200
+#define OMW_TIMER 0
+
+// Some Constants
+#define Window_Samples 5000
+#define UPPER_ACCEL_THRESHOLD 200
 #define LOWER_ACCEL_THRESHOLD 800
 #define DOOR_THRESHOLD 500000
 #define RMS_CAP 10000
 #define ONE_MIN 60000
 
+
+// NEED TO BE CHANGED FOR INDIVIDUAL DEVICES!!!
+String NFC_ID = "04457C8A6F4080";
 const char* ssid = "Stanford Residences";
 
 //const char* ssid = "washiato";
+//const char* pwd = "washiato";
 
+// Module level variables
 static String MAC_address = "";
 static int MachineState = MACHINE_UNOCCUPIED;
-MMA8452Q accel;
-LiquidCrystal lcd(0, 2, 12, 13, 14, 16);
 static unsigned long lastEvent;
+static unsigned long lastOMWTime;
+static unsigned long lastCompleteTime;
 static bool isMoving;
 static bool door_close;
+static bool lastOMWStatus;
+MMA8452Q accel;
+LiquidCrystal lcd(0, 2, 12, 13, 14, 16);
 
+// Initialization function
 void setup() {
   Serial.begin(115200);
   delay(100);
@@ -42,11 +55,21 @@ void setup() {
 
   //Set up GPIO
   pinMode(BACKLIGHT_PIN, OUTPUT);
+
+  // set up the LCD's number of columns and rows:
+  lcd.begin(16, 2);
+  // Print a message to the LCD.
+  lcd.print("Connecting to: ");
+  lcd.setCursor(0,1);
+  lcd.print(ssid);
+  // Turn on backlight
+  digitalWrite(BACKLIGHT_PIN, HIGH);
   
   //Connect to wifi
+  //WiFi.begin(ssid, pwd);
   WiFi.begin(ssid);
 
-    // Get MAC address for use as a unique ID
+  // Get MAC address for use as a unique ID
   byte MAC_array[6];
   WiFi.macAddress(MAC_array);
   for (int i=0; i < sizeof(MAC_array); ++i) {
@@ -61,6 +84,7 @@ void setup() {
     delay(500);
     Serial.print(".");
   }
+  
   // Print WiFi data for debugging
   Serial.println("");
   Serial.println("WiFi connected");  
@@ -70,25 +94,31 @@ void setup() {
   //Set up firebase object
   Firebase.begin("washiato.firebaseIO.com", "N1tSzx0jsBjsBrbpnmHL8Gn3INnlwyrkbyMq110v");
   Serial.println("Firebase Successfully Initialized");
-  
+
   // Set up accelerometer object
+  lcd.clear();
+  lcd.print("Init Accel");
   accel.init(SCALE_2G, ODR_800);
   Serial.println("Accelerometer Successfully Initialized");
 
-  // set up the LCD's number of columns and rows:
-  lcd.begin(16, 2);
-  // Print a message to the LCD.
-  lcd.print("Washiato!");
-  
+  // Set FB and LCD statuses
   setFBStatus(MACHINE_UNOCCUPIED);
+  setLCDStatus(MACHINE_UNOCCUPIED);
+  digitalWrite(BACKLIGHT_PIN, LOW);  
 }
 
+// Main loop containing SM
 void loop() {
+  
  int NextMachineState = MachineState;
+ // Update Acceleration values first
  checkMovement();
- 
+
+ // Machine State machine
  switch(MachineState) {
+  
   case MACHINE_UNOCCUPIED:
+    // If we have detected that it is moving, go to washing state
     if (isMoving) {
       Serial.println("Now Washing");
       NextMachineState = MACHINE_WASHING;
@@ -97,7 +127,9 @@ void loop() {
       lastEvent = millis();
     }
     break;
+  
   case MACHINE_WASHING:
+    // If the machine is no longer moving, go to finished state
     if (!isMoving) {
       Serial.println("Now Finished");
       NextMachineState = MACHINE_FINISHED;
@@ -105,19 +137,46 @@ void loop() {
       setLCDStatus(MACHINE_FINISHED);
       digitalWrite(BACKLIGHT_PIN, HIGH);
       lastEvent = millis();
+      lastCompleteTime = millis();
+      setFBTime(0);
     }
     break;
+    
   case MACHINE_FINISHED:
-    if (door_close) {
-      Serial.println("Now Unoccupied");
-      NextMachineState = MACHINE_UNOCCUPIED;
-      setFBStatus(MACHINE_UNOCCUPIED);
-      setLCDStatus(MACHINE_UNOCCUPIED);
-      digitalWrite(BACKLIGHT_PIN, LOW);
-      lastEvent = millis();
-    }
+      // If a door close has been detected, go to unoccupied state
+      if (door_close) {
+        Serial.println("Now Unoccupied");
+        NextMachineState = MACHINE_UNOCCUPIED;
+        setFBStatus(MACHINE_UNOCCUPIED);
+        setLCDStatus(MACHINE_UNOCCUPIED);
+        resetOMWStatus();
+        digitalWrite(BACKLIGHT_PIN, LOW);
+        lastEvent = millis();
+        lastOMWStatus = false;
+        setFBTime(0);
+      }
+
+     // If more than a second has passed since we checked if someone is on the way
+     if (millis() - lastOMWTime > 1000) {
+      // Check if someone is on the way
+      if (checkOMWStatus() && lastOMWStatus == false) {
+        lcd.clear();
+        lcd.print("I'm on my way!");
+        lastEvent = millis();
+        lastOMWStatus = true;
+      }
+      lastOMWTime = millis();
+     }
+
+     // If we have recently rolled over a minute, update firebase
+     if (((millis()-lastCompleteTime)%60000) < 20) {
+      setFBTime((uint16_t)((millis()-lastCompleteTime)/60000));
+      Serial.println("Setting FB Time");
+     }
     break;
  }
+ 
+ // Set the time line on the LCD
  setLCDTime();
  MachineState = NextMachineState;
 }
@@ -176,10 +235,6 @@ void checkMovement(void) {
       y_s = y_s - (y_s/(Window_Samples)) + (y_diff/(Window_Samples));
       z_s = z_s - (z_s/(Window_Samples)) + (z_diff/(Window_Samples));
     }
-   
-    //Serial.print((String)sq(new_x-x_mu) + "\t");
-    //Serial.print((String)sq(new_y-y_mu) + "\t");
-    //Serial.print((String)sq(new_z-z_mu) + "\t");
 
     //Serial.println(sqrt(sq(x_s)+sq(y_s)+sq(z_s)));
     
@@ -200,19 +255,21 @@ void checkMovement(void) {
 
 // Function to set the status of the washing machine in FB
 void setFBStatus(int newStatus) {
-  Firebase.set("Machines/" + MAC_address + "/Status", newStatus);
+  Firebase.set("Machines/" + NFC_ID + "/status", newStatus);
 }
 
+// Function to set the time on the lcd panel since the "last event" variable has been updated
 void setLCDTime(void) {
   lcd.setCursor(0,1);
   unsigned long timeDiff = millis()-lastEvent;
   if (timeDiff<ONE_MIN) {
     lcd.print((String)(timeDiff/1000) + " sec ago");
   } else {
-    lcd.print((String)(timeDiff/60000) + " min " + (String)((timeDiff%60000)/1000) + " sec ago");
+    lcd.print((String)(timeDiff/60000) + " m " + (String)((timeDiff%60000)/1000) + " s ago ");
   }
 }
 
+// Function to clear the LCD screen and set the status line of the LCD display
 void setLCDStatus(int newStatus) {
   switch (newStatus) {
     case MACHINE_UNOCCUPIED:
@@ -231,5 +288,20 @@ void setLCDStatus(int newStatus) {
       lcd.print("Machine finished");
     break;
   }
+}
+
+// Function to reset the on my way status for the machine in Firebase
+void resetOMWStatus(void) {
+  Firebase.set("Machines/" + NFC_ID + "/omw", false);
+}
+
+// Returns true if the clothing owner has responded in the app
+bool checkOMWStatus(void) {
+  return (bool)Firebase.get(("Machines/" + NFC_ID + "/omw").c_str());
+}
+
+// Takes a time in minutes since the last cycle completed and posts it to firebase
+void setFBTime(uint16_t newMins) {
+  Firebase.set("Machines/" + NFC_ID + "/time", newMins);
 }
 
